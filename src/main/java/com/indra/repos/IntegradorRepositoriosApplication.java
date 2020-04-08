eaf926b9eeeeddcf21e95acfc73c30b86d8a5534
Ricardo Silvestre <ricardojppb@gmail.com>
2020-04-08 00:15:13 -0300

    Init Project: Integrador de Repositorios

---
+++ src/main/java/com/indra/repos/IntegradorRepositoriosApplication.java
@@ -0,0 +1,148 @@
+package com.indra.repos;
+
+
+import com.cdancy.bitbucket.rest.BitbucketClient;
+import com.cdancy.bitbucket.rest.features.BranchApi;
+import com.cdancy.bitbucket.rest.features.CommitsApi;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.net.URI;
+import java.nio.charset.Charset;
+
+@Slf4j
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class IntegradorRepositoriosApplication implements CommandLineRunner {
+    RestTemplate restTemplate = new RestTemplate();
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegradorRepositoriosApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+//        System.out.println("*********** Init Clone ***********");
+//        String url = "http://10.10.253.35/gti/ATFWS.git";
+//        String[] projeto = url.split("/");
+//        File dir = new File("/home/ricardo/ambiente/workspace/integrador-repositorios/repos/" + projeto[projeto.length - 1]);
+//        CredentialsProvider cp = new UsernamePasswordCredentialsProvider("ricardo.silva", "048LLrass265");
+//        Git git = null;
+//        if (!(dir.exists() && dir.isDirectory())) {
+//
+//            git = Git.cloneRepository()
+//                    .setURI(url)
+//                    .setDirectory(dir)
+//                    .call();
+//        } else {
+//            git = Git.open(dir);
+//        }
+
+
+//        Map<String, Ref> asMapRefs = git.getRepository().getAllRefs();
+//        asMapRefs.forEach((k,v) -> {
+//            System.out.print("key: " + k);
+//            System.out.print(" - ");
+//            System.out.println("value: " + v.getName());
+//            System.out.println("*****************************************************************************");
+//        });
+
+//        Map<String, Ref> asMapTags = git.getRepository().getTags();
+//        asMapTags.forEach((k,v) -> {
+//            System.out.print("key: " + k);
+//            System.out.print(" - ");
+//            System.out.print("value: " + v.toString());
+//            System.out.println("*****************************************************************************");
+//        });
+//        refs.forEach(b -> {
+//            System.out.print("Branch: " + b.getName());
+//            System.out.println("*****************************************************************************");
+//        });
+//
+//        Iterable<RevCommit> logs = git.log().all().call();
+//        logs.forEach(e -> {
+//            System.out.print("*************");
+//            System.out.print("FullMessage: " + e.getFullMessage());
+//            System.out.print("*************");
+//            System.out.println("#############################################################################");
+//            System.out.print("Name: " + e.getAuthorIdent().getName());
+//            System.out.print(" - ");
+//            System.out.print("Email: " + e.getAuthorIdent().getEmailAddress());
+//            System.out.print(" - ");
+//            System.out.print("Date: " + e.getAuthorIdent().getWhen().toString());
+//            System.out.print(" - ");
+//            System.out.println("Id: " + e.getId().toObjectId().getName());
+//            System.out.println("*****************************************************************************");
+//
+//        });
+
+        getAllCommitsBitBucket();
+    }
+
+    public void getAllCommitsBitBucket() {
+
+//        BitbucketClient client = BitbucketClient.builder()
+//                .endPoint("https://bitbucket.indra.es") // Optional and can be sourced from system/env. Falls back to http://127.0.0.1:7990
+//                .token("NzU0MjE0Nzg0Njk2Ouxl5cMM3a11g6W5dPtQmM7WMu1N") // Optional and can be sourced from system/env and can be Base64 encoded.
+//                .build();
+//
+//        BranchApi branchs = client.api().branchApi();
+//        CommitsApi commitsApi = client.api().commitsApi();
+//
+        System.out.println("*********** Get All Commits for Rest ***********");
+        String url = "https://bitbucket.indra.es/rest/api/1.0/" +
+                "projects/jpa/repos/sefazpb-atf/commits?exclude=master&limit=1000";
+        String accessToken = "Bearer NzU0MjE0Nzg0Njk2Ouxl5cMM3a11g6W5dPtQmM7WMu1N";
+        String auth = "Basic rasilvestres:048265LLrass022020";
+
+//        HttpHeaders httpHeaders = restTemplate.headForHeaders(url);
+//        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+//        //httpHeaders.setBearerAuth("NzU0MjE0Nzg0Njk2Ouxl5cMM3a11g6W5dPtQmM7WMu1N");
+//        //httpHeaders.add("Authorization","Basic "+ "NzU0MjE0Nzg0Njk2Ouxl5cMM3a11g6W5dPtQmM7WMu1N");
+//        //httpHeaders.setBasicAuth("rasilvestres","048265LLrass022020");
+//
+//        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+//        headers.add("Authorization", auth);
+//        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+//
+//        RequestEntity<Object> request = new RequestEntity<>(
+//                headers, HttpMethod.GET, URI.create(url));
+//
+//        ResponseEntity<String> response
+//                = restTemplate.exchange(request,String.class);
+
+        ResponseEntity<String> response = restTemplate.exchange
+                (url, HttpMethod.GET, new HttpEntity<String>(createHeaders("rasilvestres", "048265LLrass022020")), String.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            System.out.println("Status:" + HttpStatus.OK.name());
+            System.out.println("Body:" + response.getBody());
+        }
+
+        restTemplate = null;
+
+    }
+
+    private HttpHeaders createHeaders(String username, String password){
+        return new HttpHeaders() {{
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.encodeBase64(
+                    auth.getBytes(Charset.forName("US-ASCII")) );
+            String authHeader = "Basic " + new String( encodedAuth );
+            set( "Authorization", authHeader );
+        }};
+    }
+}
---
+++ src/test/java/com/indra/repos/IntegradorRepositoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indra.repos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorRepositoriosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
